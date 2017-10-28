2d51f037fda7ec2fb25143b7c39886d559581b7a
Gokulan <gokulan97@gmail.com>
2017-10-28 18:52:26 +0530

    updated navbar and mongodb name

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,6 +19,6 @@ module.exports = {
     uri: process.env.MONGODB_URI
       || process.env.MONGOHQ_URL
       || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME
-      || 'mongodb://localhost/researchcon'
+      || 'mongodb://' + process.env.RCON_USER + ':' + process.env.RCON_PASSWORD + '@localhost/research-conference'
   }
 };
