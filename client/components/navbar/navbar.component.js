0d741b68d3839766b87678fefa20a6c0669d5c5c
Gokulan <gokulan97@gmail.com>
2017-11-05 03:12:11 +0530

    final changes

--- client/components/navbar/navbar.component.js
+++ client/components/navbar/navbar.component.js
@@ -13,6 +13,7 @@ export class NavbarComponent {
     this.isLoggedIn = Auth.isLoggedInSync;
     this.isAdmin = Auth.isAdminSync;
     this.getCurrentUser = Auth.getCurrentUserSync;
+    this.isCollapsed = true;
   }
 
 }
